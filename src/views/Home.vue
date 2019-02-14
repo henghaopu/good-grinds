9480a1bf2603fc36a80c16617679d9b3557be4c4
Heng-Hao Pu <hpu@oswego.edu>
2019-02-14 16:04:15 -0500

    display local data in Home vue component in a responsive card form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="home">
-    
+  <div class="home container">
+    <div class="card" v-for="coffee in coffeeList" :key="coffee.id">
+      <div class="card-content">
+        <h2 class="brown-text">{{coffee.name}}</h2>
+        <ul class="ingredients">
+          <li v-for="(ingredient, index) in coffee.ingredients" :key="index">
+            <span class="chip">{{ingredient}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +19,12 @@ export default {
   name: 'home',
   data() {
     return {
-      
+      coffeeList: [
+        {id: '1', name: 'Espresso', subPath: 'espresso', ingredients: []},
+        {id: '2', name: 'Americano', subPath: 'americano', ingredients: ['Espresso', 'Water']},
+        {id: '3', name: 'Latte', subPath: 'latte', ingredients: ['Espresso', 'Steamed Milk', 'Milk Foam']}, 
+        {id: '4', name: 'Mocha', subPath: 'mocha', ingredients: ['Espresso', 'Steamed milk', 'Milk Foam', 'Chocolate powder']}
+      ]
     }
   },
   components: {
@@ -20,6 +34,26 @@ export default {
 </script>
 
 <style scoped>
+.home {
+  display: grid;
+  /* grid-template-columns: 1fr 1fr 1fr; */
+  grid-template-columns: repeat( auto-fit, minmax(250px,1fr));  
+  grid-gap: 1.875rem;
+  margin-top: 3.75rem;
+}
 
+.home h2 {
+  font-size: 1.8rem;
+  text-align: center;
+  margin-top: 0;
+}
+
+.home .ingredients {
+  margin: 30px auto;
+}
+
+.home .ingredients li {
+  display: inline-block;
+}
 </style>
 
