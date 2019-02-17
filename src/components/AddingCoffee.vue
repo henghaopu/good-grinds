f141756ba372c88c997fa12066934618ad0126a5
Heng-Hao Pu <hpu@oswego.edu>
2019-02-17 00:50:28 -0500

    [dynamic form] [accessibility] add an ingredient and then update local data and ui; v-model binding technique; link label and input together

--- src/components/AddingCoffee.vue
+++ src/components/AddingCoffee.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="add-coffee container">
-        <h2 class="cneter-align brown-text">Add New Coffee Recipe</h2>
+        <h2 class="center-align brown-text">Add New Coffee Recipe</h2>
         <form @submit.prevent="addCoffee">
             <div class="field title">
                 <label for="title">Coffee Title:</label>
-                <input type="text" name="title" v-model="title">
+                <input type="text" id="title" v-model="title">
+            </div>
+            <div class="field added-ingredient" v-for="(ingredient, index) in addedIngredients" :key="index">
+                <label :for="index">Added Ingredient</label>
+                <!-- <input type="text" :value="ingredient"> -->
+                <input type="text" :id="index" v-model="addedIngredients[index]"> 
             </div>
             <div class="field add-ingredient">
                 <label for="add-ingredient">Add an Ingredient:</label>
-                <input type="text" name="add-ingredient">
+                <div class="input-icon-wrap">
+                    <input type="text" id="add-ingredient" v-model="newIngredient">
+                    <i class="material-icons" tabindex="0" @click="addIngredient" @keyup.enter="addIngredient">add_circle_outline</i>
+                </div>
             </div>
             <div class="field center-align">
                 <button class="btn orange darken-1">Add Coffee</button>
@@ -23,12 +31,20 @@ export default {
     name: 'AddingCoffee',
     data() {
         return {
-            title: null
+            title: null,
+            newIngredient: null,
+            addedIngredients: []
         }
     },    
     methods: {
         addCoffee() {
-            console.log('Coffee Title: ' + this.title)
+            console.log('Coffee Name: ' + this.title + '\n Coffee Ingredients: ' + this.addedIngredients)
+        },
+        addIngredient() {
+            if (this.newIngredient) {
+                this.addedIngredients.push(this.newIngredient)
+                this.newIngredient = null
+            }
         }
     }
 }
@@ -46,6 +62,22 @@ export default {
 }
 .add-coffee .field {
     margin: 1.25rem auto;
+}
+.input-icon-wrap {
+    position: relative;
+}
+.input-icon-wrap i {
+    position: absolute;
+    right: 0px;
+    color: hsl(0, 0%, 60%);
+    cursor: pointer;
+}
+.input-icon-wrap i:hover {
+    color: hsl(0, 0%, 45%);
+}
+.input-icon-wrap i:focus {
+    outline: 2px dashed pink;
+    color: hsl(171, 17%, 51%);
 }
 button:focus {
     outline: 2px dashed pink;
