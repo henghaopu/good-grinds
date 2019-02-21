f63f05517c09cae401b92a889d917c77ab5c5d5e
Heng-Hao Pu <hpu@oswego.edu>
2019-02-21 13:51:48 -0500

    [database] query data from Firebase

--- src/components/EditingCoffee.vue
+++ src/components/EditingCoffee.vue
@@ -1,17 +1,29 @@
 <template>
-    <div class="container edit-coffee">
-        <h2>Edit a Coffee {{ this.$route.params.coffee_path }}</h2>
+    <div v-if="coffee" class="container edit-coffee">
+        <h2>Edit {{coffee.name}}</h2>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
 export default {
     name: 'EditingCoffee',
     data() {
         return {
-            
+            coffee: null
         }
     },
+    // called when the component is created but not yet render to the DOM
+    created() {
+        // issue: why can't we use '==='? 
+        let ref = db.collection('coffeeCollection').where('subPath', '==', this.$route.params.coffee_path)
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.coffee = doc.data()
+                this.coffee.if = doc.id
+            })
+        })
+    }
 }
 </script>
 
